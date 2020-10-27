ca1a64af113ca8c21b433abf5472e0e92a0df70a
midhun-mohan <53261270+midhun-mohan@users.noreply.github.com>
2020-10-27 16:56:09 +0100

    add naming convention provider class

---
+++ poc-pulumi-csharp/namingConventionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pulumi_poc_midhun
+{
+    public class NamingConventionProvider
+    {
+        private const string owner = "midhun";
+        
+        public static string Resourcegroup(string stack)
+        {
+            return $"{owner}-{stack}-new-rg";
+        }
+
+        public static string StorageAccount(string stack)
+        {
+            return $"{owner}{stack}newsa";
+        }
+
+        public static string Keyvault(string stack)
+        {
+            return $"{owner}-{stack}-new-vault";
+        }
+        
+        public static string GetServiceBusNamespace(string stack)
+        {
+            return $"{owner}-{stack}-sbns";
+        }
+        
+        public static string GetServiceBusQueue(string stack)
+        {
+            return $"{owner}-{stack}-sbqueue";
+        }
+        
+        public static string GetServiceBusAuthRuleName(string stack, string ruleType)
+        {
+            return $"{owner}-{stack}-sbns-{ruleType}";
+        }
+    }
+}
